2049a26e37bff61312b7607e4e5a960b06012433
Bruna Nunes da Silva <silvabruna.email@gmail.com>
2021-06-23 19:06:03 -0300

    android

--- Agenda/app/src/main/java/alura/com/br/agenda/MainActivity.java
+++ Agenda/app/src/main/java/alura/com/br/agenda/MainActivity.java
@@ -1,16 +1,29 @@
 package alura.com.br.agenda;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Hello World!", Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Sonia", "Bruna"));
+        ListView listaDeAlunos = findViewById(R.id.id_listadealunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
+
     }
 }
